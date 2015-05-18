2f9aa46ecc65834e22cbf6f72a91280489f42f2b
Ernest Lijoi <ernielijoi@gmail.com>
2015-05-18 14:03:45 -0700

    Initial Commit

---
+++ code/php/footer.php
@@ -0,0 +1,11 @@
+<div id="footer">
+    <div class="container">
+        <div id="footer_curtain">
+
+        </div>   
+    </div>
+</div>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="code/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
